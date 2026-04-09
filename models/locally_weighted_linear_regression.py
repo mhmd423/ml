246e03073506f3ac22c2d39bd197515ef71ba403
Mohammed Ahmed <mhmdgapper@gmail.com>
2026-04-09 19:31:30 +0200

    refactor: improve the fit method

--- models/locally_weighted_linear_regression.py
+++ models/locally_weighted_linear_regression.py
@@ -5,22 +5,33 @@ from .base import Model
 
 
 class lw_LinearRegression(Model):
-    def __init__(self):
+    def __init__(self, tau=None, lamda=1e-5):
         super().__init__()
         self.theta = None
-        self.tau = None
+        self.tau = tau
+        self.lamda = lamda
         self.X_train = None
         self.y_train = None
-        
-    def fit(self, X, y, tau):
-        self.tau = tau
-        self.X_train = X
+
+    def fit(self, X, y, tau=None, fit_intercept=True, standardize=False, lamda=None):
+        if tau is not None:
+            self.tau = tau
+        if lamda is not None:
+            self.lamda = lamda
+
+        if self.tau is None or self.tau <= 0:
+            raise ValueError("tau must be a positive number")
+
+        X = np.asarray(X, dtype=float)
+        y = np.asarray(y, dtype=float).reshape(-1, 1)
+
+        self._add_intercept = fit_intercept
+        self._standardize = standardize
+        self.mu = X.mean(axis=0)
+        self.sigma = X.std(axis=0)
+
+        self.X_train = self.preprocess(X)
         self.y_train = y
-    
-    
-    
-        
-    
 
         return self
 
