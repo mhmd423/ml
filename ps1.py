bee9f0b1495e98fc1c04202d3d34f779820737f8
Mohammed Ahmed <mhmdgapper@gmail.com>
2026-04-10 17:45:04 +0200

    feat: make a file for problem set 1

---
+++ ps1.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from metrics import accuracy_score
+from models import GDA, LogisticRegression, PoissonRegression
+
+#parent of all the data
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
